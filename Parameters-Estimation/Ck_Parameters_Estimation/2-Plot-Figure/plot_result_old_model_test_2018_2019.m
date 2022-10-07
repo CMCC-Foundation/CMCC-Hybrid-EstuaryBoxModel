5886285381c7a9200618cf1059cae31da9b9de1e
LeonardoSaccotelli <leosaccotelli96@gmail.com>
2022-10-07 16:22:50 +0200

    Updated script to plot results on ck experiment

---
+++ Parameters-Estimation/Ck_Parameters_Estimation/2-Plot-Figure/plot_result_old_model_test_2018_2019.m
@@ -0,0 +1,20 @@
+addpath(genpath('..\0-Dataset\'));
+addpath(genpath('..\1_Trained-Models\training_2016_2017_test_2018_2019_comparing_old_model'));
+addpath(genpath('..\..\..\Machine-Learning-Tools\3-Plot-Figure'));
+load("..\0-Dataset\CK_OLD_MODEL_PREDICTIONS.mat")
+load("..\1-Trained-Models\training_2016_2017_test_2018_2019_comparing_old_model\Ck-Trained-Tested-model-k-10-old-configuration.mat");
+
+algorithm_names = {'old model','random forest', 'lsboost', 'neural network'};
+response = 'CK_Obs';
+
+%% Test dataset
+test_table_results = array2table([ck_dataset.CK_Obs(ck_dataset.Year == 2018 | ck_dataset.Year == 2019 )...
+    ck_dataset.Ck_old_model(ck_dataset.Year == 2018 | ck_dataset.Year == 2019)...
+    result_trained_model.random_forest.test_results.test_2018_2019_dataset.test_predictions...
+    result_trained_model.lsboost.test_results.test_2018_2019_dataset.test_predictions...
+    result_trained_model.neural_network.test_results.test_2018_2019_dataset.test_predictions ...
+],"VariableNames",{'real_ck', 'old_model_pred', 'rf_pred', 'lsb_pred', 'nn_pred'});
+
+create_perfect_fit_residuals_plot(test_table_results, algorithm_names, response, "Test dataset 2018 - 2019");
+compare_real_pred_obs(test_table_results, algorithm_names, "Test dataset 2018 - 2019", "ck");
+
