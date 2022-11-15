de31ba1a81b0218707c51a18df6bb3fea8e75a46
LeonardoSaccotelli <leosaccotelli96@gmail.com>
2022-11-15 16:23:32 +0100

    Added bar plot script - added script 'plot_bar_q_river_splitted_obs_by_year' - added script 'plot_bar_q_river_splitted_obs_by_trainint_test_year'

---
+++ Machine-Learning-Tools/3-Plot-Figure/plot_bar_q_river_splitted_obs_by_training_test_year.m
@@ -0,0 +1,37 @@
+function plot_bar_q_river_splitted_obs_by_training_test_year(dataset)
+    result = zeros(4,2);
+    
+    % training Q_River_LOW
+    result(1,1) = height(dataset((dataset.Year == 2016 | dataset.Year == 2017) ...
+        & strcmp(string(dataset.Q_river_class), "LOW"),:));
+    % training Q_River_STRONG
+    result(1,2) = height(dataset((dataset.Year == 2016 | dataset.Year == 2017) ...
+        & strcmp(string(dataset.Q_river_class), "STRONG"),:));
+
+    % test 2018 Q_River_LOW
+    result(2,1) = height(dataset((dataset.Year == 2018) ...
+        & strcmp(string(dataset.Q_river_class), "LOW"),:));
+    % test 2018 Q_River_STRONG
+    result(2,2) = height(dataset((dataset.Year == 2018) ...
+        & strcmp(string(dataset.Q_river_class), "STRONG"),:));
+
+        % test 2019 Q_River_LOW
+    result(3,1) = height(dataset((dataset.Year == 2019) ...
+        & strcmp(string(dataset.Q_river_class), "LOW"),:));
+    % test 2019 Q_River_STRONG
+    result(3,2) = height(dataset((dataset.Year == 2019) ...
+        & strcmp(string(dataset.Q_river_class), "STRONG"),:));
+    
+    figure();
+    hAx=gca;
+    bar(result);
+    hAx.LineWidth=1;
+    ylim([0 500]);
+    xlabel('Year');
+    ylabel('Observations');
+    xticks([1,2,3]);
+    xticklabels({'Training 2016-2017', 'Test 2018', 'Test 2019'});
+    title('Number of observations by training/test for q-river-class');
+    legend('Low','Strong','Location','northeast');
+    set(gca,'FontSize',12);
+end
---
+++ Machine-Learning-Tools/3-Plot-Figure/plot_bar_q_river_splitted_obs_by_year.m
@@ -0,0 +1,25 @@
+function plot_bar_q_river_splitted_obs_by_year(dataset)
+    year_obs = unique(dataset.Year);
+    result = zeros(4,2);
+    
+    for i = 1:numel(year_obs)
+        % Q_River_LOW
+        result(i,1) = height(dataset(dataset.Year == year_obs(i) & strcmp(string(dataset.Q_river_class), "LOW"),:));
+
+        % Q_River_STRONG
+        result(i,2) = height(dataset(dataset.Year == year_obs(i) & strcmp(string(dataset.Q_river_class), "STRONG"),:));
+    end
+
+    figure();
+    hAx=gca;
+    bar(result);
+    hAx.LineWidth=1;
+    ylim([0 500]);
+    xlabel('Year');
+    ylabel('Observations');
+    xticks(linspace(1, numel(year_obs), numel(year_obs)));
+    xticklabels(string(year_obs));
+    title('Number of observations by year for q-river-class');
+    legend('Low','Strong','Location','northeast');
+    set(gca,'FontSize',12);
+end
