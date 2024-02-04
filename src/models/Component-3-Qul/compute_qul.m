38490d4bb8947c8b0ac9dfa3809273e3cea521f6
LeonardoSaccotelli <leonardo.saccotelli@cmcc.it>
2024-02-04 17:56:25 +0100

    updated repository structure

--- src/models/Component-3-Qul/compute_qul.m
+++
@@ -1,4 +0,0 @@
-% Function to compute the Component-3 of Hybrid-EBM
-function Qul = compute_qul(Qriver, Qll, Qtide)
-    Qul = Qriver + Qll + Qtide;
-end
--- src/models/Component-4-Sul/compute_sul.m
+++
@@ -1,6 +0,0 @@
-% Function to compute the Component-4 of Hybrid-EBM
-function sul = compute_sul(Sll, Qll, Socean, Qtide, Ck, Ly, utidef, H, Lx, Qriver)
-    Lx = Lx*1000;
-    sul_qul = Sll*Qll + Socean*Qtide + Ck*Ly*utidef*H*Ly*(Socean/Lx);
-    sul = sul_qul / compute_qul(Qriver,Qll,Qtide);
-end
